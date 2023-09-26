c53b6d92f60c17ffa1220a1192194d3f25ddbb03
caleberebello <caleberoberto2@gmail.com>
2023-09-26 15:39:41 -0400

    Alterando visual

--- nodejs/database.js
+++ nodejs/database.js
@@ -22,7 +22,7 @@ app.get("/teste", async(req, res) => {
         res.send(jsonS);
     }
     catch(e){
-
+        console.log(e);
     }
 });
 
--- svelte/my-aoo/src/routes/+page.svelte
+++ svelte/my-aoo/src/routes/+page.svelte
@@ -1,17 +1,40 @@
 <script>
-    let teste = ""
+    let teste = "";
 
     async function retornaBanco() {
         try{
             const response = await fetch("http://localhost:4003/teste");
             const data = await response.json();
-            teste = JSON.stringify(data);
+            teste = data;
         }
         catch(e) {
             teste = "Erro";
+            console.log(e);
         }
     }
 </script>
 
 <button on:click={retornaBanco}>Teste</button>
-<p>{teste}</p>
+{#each teste as testes (testes.id)}
+    <div class="card">
+        <h2>Nome: {testes.nome}</h2>
+        <p>Idade: {testes.idade}</p>
+    </div>
+{/each}
+
+<style>
+    @import url('https://fonts.googleapis.com/css2?family=Poppins&display=swap');
+
+    .card {
+        border: 1px solid #ccc;
+        padding: 10px;
+        margin: 10px;
+        border-radius: 5px;
+        box-shadow: 2px 2px 5px rgba(0,0,0,0.2);
+    }
+
+    h2,p {
+        font-family: 'Poppins', sans-serif;
+        font-size: 20px;
+    }
+</style>
